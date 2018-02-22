275e24531959760176bf4ab392ddc465e636a257
Ivan Ananev <ivcheg@gmail.com>
2018-02-22 17:11:02 +0300

    увеличение кол-во воркеров для обработки

--- lib/sender/core/email/msg_handler_supervisor.ex
+++ lib/sender/core/email/msg_handler_supervisor.ex
@@ -2,5 +2,5 @@ defmodule Sender.Core.Email.MsgHandlerSupervisor do
   use Sender.Core.Base.MsgHandlerSupervisor,
     queue_handler: Sender.Core.Email.QueueHandler,
     msg_handler: Sender.Core.Email.MsgHandler,
-    max_demand: 10
+    max_demand: 1_000
 end
--- lib/sender/core/sms/msg_handler_supervisor.ex
+++ lib/sender/core/sms/msg_handler_supervisor.ex
@@ -2,5 +2,5 @@ defmodule Sender.Core.Sms.MsgHandlerSupervisor do
   use Sender.Core.Base.MsgHandlerSupervisor,
     queue_handler: Sender.Core.Sms.QueueHandler,
     msg_handler: Sender.Core.Sms.MsgHandler,
-    max_demand: 10
+    max_demand: 1_000
 end
